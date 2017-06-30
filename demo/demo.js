9f3d736c436b9b304991146b878a31de178cddfd
wasd2017 <820107947@qq.com>
2017-06-30 17:22:30 +0800

    finish
    finfsh2017/6/30  17:22

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     $("div.h1").click(function() {
-        $("#c2").hide();
+        $("#c2").toggle();
+    });
+    $("#t7").click(function() {
+        window.print();
+    });
+    $("#t6").click(function() {
+        parent.location = 'mailto:iazxq@sohu.com?subject=欢迎分享JavaScript';
     })
 });
